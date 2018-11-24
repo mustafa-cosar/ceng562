25c5abd7153a9d47d23610fb8298e58f47f8d0b9
Mustafa Senol Cosar <cosar.mustafasenol@gmail.com>
2018-11-24 18:35:16 +0300

    Train a neural network and make predictions

--- src/neural.py
+++ src/neural.py
@@ -2,16 +2,30 @@ import tensorflow as tf
 from tensorflow import keras
 
 
-def run(data_files, model_parameters):
-    reader = tf.data.TextLineDataset(data_files)
+def run_nn(training_data, training_labels, test_data, test_labels, prediction_set=None, epochs=10):
+
+    input_count = training_data.shape[1]
+    output_count = training_labels.shape[1]
+    print(input_count, output_count)
 
     with tf.Session() as session:
         model = keras.Sequential([
-            keras.layers.,
+            keras.layers.Dense(input_count),
+            keras.layers.Dense(256, activation=tf.nn.relu),
             keras.layers.Dense(128, activation=tf.nn.relu),
-            keras.layers.Dense(128, activation=tf.nn.relu),
-            keras.layers.Dense(6, activation=None)
+            keras.layers.Dense(64, activation=tf.nn.relu),
+            keras.layers.Dense(output_count, activation=None)
         ])
 
+        model.compile(
+            optimizer=tf.train.AdamOptimizer(), loss='mean_squared_error', metrics=['accuracy'])
+
+        model.fit(training_data, training_labels, epochs=epochs)
+
+        test_loss, test_accuracy = model.evaluate(test_data, test_labels)
+
+        if prediction_set is not None:
+            return model.predict(prediction_set)
 
 
+        return model, test_loss, test_accuracy
